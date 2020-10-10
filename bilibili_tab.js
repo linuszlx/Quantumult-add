e20785f207187e20eb2c5661900736bad9b59a3d
linuszlx <linuszhang@126.com>
2020-10-10 14:27:29 +0800

    Update bilibili_tab.js

--- bilibili_tab.js
+++ bilibili_tab.js
@@ -52,43 +52,41 @@ body['data']['top'] = [
 ]
 
 body['data']['bottom'] = [
-    {
-        "id": 102,
-        "icon": "http://i0.hdslb.com/bfs/archive/63d7ee88d471786c1af45af86e8cb7f607edf91b.png",
-        "icon_selected": "http://i0.hdslb.com/bfs/archive/e5106aa688dc729e7f0eafcbb80317feb54a43bd.png",
-        "name": "首页",
-        "uri": "bilibili://main/home/",
-        "tab_id": "首页Bottom",
-        "pos": 1
-    },
-    {
-        "id": 103,
-        "icon": "http://i0.hdslb.com/bfs/archive/9c453a54eb83f5140cd098bf2e8ed8a599edc7fe.png",
-        "icon_selected": "http://i0.hdslb.com/bfs/archive/79d29e6ac3b6e52652881b050e63988e2038130f.png",
-        "name": "频道",
-        "uri": "bilibili://pegasus/channel/",
-        "tab_id": "频道Bottom",
-        "pos": 2
-    },
-    {
-        "id": 104,
-        "icon": "http://i0.hdslb.com/bfs/archive/86dfbe5fa32f11a8588b9ae0fccb77d3c27cedf6.png",
-        "icon_selected": "http://i0.hdslb.com/bfs/archive/25b658e1f6b6da57eecba328556101dbdcb4b53f.png",
-        "name": "动态",
-        "uri": "bilibili://following/home/",
-        "tab_id": "动态Bottom",
-        "pos": 3
-    },
-
-    {
-        "id": 106,
-        "icon": "http://i0.hdslb.com/bfs/archive/4b0b2c49ffeb4f0c2e6a4cceebeef0aab1c53fe1.png",
-        "icon_selected": "http://i0.hdslb.com/bfs/archive/a54a8009116cb896e64ef14dcf50e5cade401e00.png",
-        "name": "我的",
-        "uri": "bilibili://user_center/",
-        "tab_id": "我的Bottom",
-        "pos": 4
-    }
+      {
+        "uri" : "bilibili:\/\/main\/home\/",
+        "tab_id" : "首页Bottom",
+        "pos" : 1,
+        "id" : 102,
+        "icon_selected" : "http:\/\/i0.hdslb.com\/bfs\/archive\/d6a45f06684562dd9cb6914007658c0cdb17bbff.png",
+        "icon" : "http:\/\/i0.hdslb.com\/bfs\/archive\/1ab5459ccb18c7a996315327257375be3da19886.png",
+        "name" : "首页"
+      },
+      {
+        "uri" : "bilibili:\/\/pegasus\/channel\/",
+        "tab_id" : "频道Bottom",
+        "pos" : 2,
+        "id" : 103,
+        "icon_selected" : "http:\/\/i0.hdslb.com\/bfs\/archive\/94539249e59621214f7dc1226cf38a2b8fe4c64f.png",
+        "icon" : "http:\/\/i0.hdslb.com\/bfs\/archive\/b4f621f268c1f9eda501805135f132aa9498b0ba.png",
+        "name" : "频道"
+      },
+      {
+        "uri" : "bilibili:\/\/following\/home\/",
+        "tab_id" : "动态Bottom",
+        "pos" : 3,
+        "id" : 104,
+        "icon_selected" : "http:\/\/i0.hdslb.com\/bfs\/archive\/1d37925562cd3e7d2e5f0868f966b5b9a8b86cde.png",
+        "icon" : "http:\/\/i0.hdslb.com\/bfs\/archive\/0f15d5f5be25af29eec6f002561d5000a77cc914.png",
+        "name" : "动态"
+      },
+      {
+        "uri" : "bilibili:\/\/user_center\/",
+        "tab_id" : "我的Bottom",
+        "pos" : 4,
+        "id" : 106,
+        "icon_selected" : "http:\/\/i0.hdslb.com\/bfs\/archive\/36e080bbd8ae858af664ef251741124e04241942.png",
+        "icon" : "http:\/\/i0.hdslb.com\/bfs\/archive\/aafe71f10eeb5086ac119e4dad769c5aad4d86a2.png",
+        "name" : "我的"
 ]
 
 body = JSON.stringify(body)
