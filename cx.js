2ee1fb2ca84c5aa955c6ba58b5952e7960898d7e
linuszlx <linuszhang@126.com>
2021-03-20 16:29:35 +0800

    Update cx.js

--- cx.js
+++ cx.js
@@ -1,3 +1,7 @@
-var body = $response.body.replace('var isPay = document', 'var isPay = 1;  #').replace('var articleProperties', 'var articleProperties2').replace('isFree\":\\d', 'isFree\":0').replace('aType\":\\d', 'aType\":0').replace('feeType\":\\d', 'feeType\":0')
+var body = $response.body.
+replace('var isPay = document', 'var isPay = 1;  #').
+replace('var articleProperties', 'var articleProperties2').
+replace('isFree\":\\d', 'isFree\":0').replace('aType\":\\d', 'aType\":0').
+replace('feeType\":\\d', 'feeType\":0')
 
 $done({ body });
