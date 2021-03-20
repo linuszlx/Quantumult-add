a23f01a5605fe4a30bade423e5e7ae2b7a77e743
linuszlx <linuszhang@126.com>
2021-03-20 16:35:27 +0800

    Update cx.js

--- cx.js
+++ cx.js
@@ -1,7 +1,8 @@
 var body = $response.body.
 replace('var isPay = document', 'var isPay = 1;  #').
 replace('var articleProperties', 'var articleProperties2').
-replace('isFree\":\\d', 'isFree\":0').replace('aType\":\\d', 'aType\":0').
+replace('isFree\":\\d', 'isFree\":0').
+replace('aType\":\\d', 'aType\":0').
 replace('feeType\":\\d', 'feeType\":0')
 
 $done({ body });
