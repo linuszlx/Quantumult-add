539228fe94f65c3d0ae62a09133aafaab365a6a7
linuszlx <linuszhang@126.com>
2021-03-24 09:35:16 +0800

    Update jd.js

--- jd.js
+++ jd.js
@@ -1,2 +1,4 @@
-var body = $response.body.replace("expo" : "6868_497#1#1_0_375862_____375862_21__", "expo" : "6868_9000#9000#900_0_375862_____375862_21__").replace("expo" : "7096_34#1#1_0_417850_____417850_215__", "expo" : "expo" : "7096_9999#1#1_0_417850_____417850_215__");
+var body = $response.body.
+replace('"showTime":2','"showTime":0').
+replace('"Time":3','"Time":0').
 $done({ body });
