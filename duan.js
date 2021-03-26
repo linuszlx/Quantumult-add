547e39659f4dbf19dd6ebac619557c63f70f0832
linuszlx <linuszhang@126.com>
2021-03-26 22:57:54 +0800

    Create duan.js

---
+++ duan.js
@@ -0,0 +1,18 @@
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj.access.profile = {
+    "is_member" : true,
+    "is_premium" : true,
+    "nickname" : "linusZ",
+    "custom_font" : true,
+    "end_member_date" :"2099-01-01",
+    "end_premium_date" :"2099-01-01",
+    "language" : "zh-hans",
+    "coin" : "0.0",
+    "invite_max" : 0,
+    "default_font_size" : "normal",
+    "current_period_end" : null
+  };
+body = JSON.stringify(obj);
+$done({body});
