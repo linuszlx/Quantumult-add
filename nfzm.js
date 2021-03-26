30b9f1baf61e33d1bf2cdbbb3689acc0d929eaba
linuszlx <linuszhang@126.com>
2021-03-26 21:21:51 +0800

    Update nfzm.js

--- nfzm.js
+++ nfzm.js
@@ -1,7 +1,7 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj.expire_time =  2023-11-11;
-obj.member_type =  5;
+obj.data.expire_time =  9999;
+obj.data.member_type =  5;
 body = JSON.stringify(obj);
 $done({body});
