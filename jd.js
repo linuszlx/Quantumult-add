17c6ed6a1c8af824a6eb2adb144981496ed3ef00
linuszlx <linuszhang@126.com>
2021-03-26 21:29:10 +0800

    Update jd.js

--- jd.js
+++ jd.js
@@ -1,34 +1,7 @@
 var body = $response.body;
 var obj = JSON.parse(body);
-obj.images=[
-    [
-      {
-        "showTimes" : 0,
-        "onlineTime" : "2022-03-15 00:00:00",
-        "referralsTime" : "2022-03-16 00:00:00",
-        "ynSkip" : 1,
-        "url" : "",
-        "time" : 0,
-        "type" : 0,
-        "sourceValueJson" : "",
-        "tag" : 0,
-        "jump" : {
-          "srv" : "",
-          "shareInfo" : {
-            "avatar" : ""
-          },
-          "des" : "m",
-          "params" : {
-            "needLogin" : "0",
-            "url" : ""
-          }
-        },
-        "miniConflict" : 0,
-        "groupId" : 9657,
-        "sequence" : 1,
-        "sourceValue" : ""
-      }
-    ]
-  ];
+obj.images.showTimes=0;
+obj.images.time=0;
+obj.images.url= "";
 body = JSON.stringify(obj);
 $done({body});
