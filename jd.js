f63634e08d349b0228683129c203b41b00145ff2
linuszlx <linuszhang@126.com>
2021-03-27 13:44:39 +0800

    Update jd.js

--- jd.js
+++ jd.js
@@ -1,7 +1,4 @@
 var body = $response.body;
 var obj = JSON.parse(body);
-obj.images.showTimes=0;
-obj.images.time=0;
-obj.images.url= "";
-body = JSON.stringify(obj);
-$done({body});
+delete obj.images;
+$done({body: JSON.stringify(obj)});
