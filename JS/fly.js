51e98a4cf4d74b284dc522e55473f64dbeaebd90
linuszlx <linuszhang@126.com>
2021-04-29 23:00:26 +0800

    Update fly.js

--- JS/fly.js
+++ JS/fly.js
@@ -1,5 +1,5 @@
 var body = $response.body;
 var obj = JSON.parse(body);
-delete obj.Variables.data.bottomadv;
+delete obj.['bottomadv'];
 delete obj.Variables.data.middleadv;
 $done({body: JSON.stringify(obj)});
