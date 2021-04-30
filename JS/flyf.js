eb71394dfa6ce55996793a3448de1080777d83c0
linuszlx <linuszhang@126.com>
2021-04-30 14:57:55 +0800

    Update flyf.js

--- JS/flyf.js
+++ JS/flyf.js
@@ -1,4 +1,7 @@
 var body = $response.body;
 var obj = JSON.parse(body);
-delete obj.Variables.data.adv;
+    if (body['Variables'].hasOwnProperty('data')){
+      if (body['Variables']['data'].hasOwnProperty('adv')){
+        delete body['Variables']['data']['adv'];
+      }
 $done({body: JSON.stringify(obj)});
