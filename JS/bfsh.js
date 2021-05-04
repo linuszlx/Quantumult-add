7190649f1ba1d0b353da6f0f3621d0f8b7cc8245
linuszlx <linuszhang@126.com>
2021-05-05 00:20:58 +0800

    Update bfsh.js

--- JS/bfsh.js
+++ JS/bfsh.js
@@ -1,5 +1,5 @@
   
 var body = $response.body;
 var obj = JSON.parse(body);
-obj.data= "{\"date\":\"2021-05-04 22:48:27\",\"result\":\"操作成功\",\"stat\":\"00\"}";
+obj.data= "{\"date\":\"2021-09-09 22:48:27\",\"result\":\"操作成功\",\"stat\":\"00\"}";
 $done({body: JSON.stringify(obj)});
