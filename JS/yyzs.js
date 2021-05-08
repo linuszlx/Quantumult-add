c9fbcfc3086a01888fef2bf0ea5c812e922f76c7
linuszlx <linuszhang@126.com>
2021-05-08 10:14:58 +0800

    Update yyzs.js

--- JS/yyzs.js
+++ JS/yyzs.js
@@ -4,7 +4,7 @@ by Primovist
 Surge:
 
 [Script]
-http-response ^https?:\/\/(i|newdrugs)\.dxy\.cn\/(snsapi\/username\/|app\/user\/(pro\/stat|init)) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/yyzs.js
+http-response ^https?:\/\/(i|newdrugs)\.dxy\.cn\/(snsapi\/username\/|app\/user\/(pro\/stat|init)) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/linuszlx/Quantumult-add/master/JS/yyzs.js
 
 [MITM]
 hostname = newdrugs.dxy.cn
