215a37ca5fe40e492af2801605437c849c2f31e1
linuszlx <linuszhang@126.com>
2021-07-06 11:44:56 +0800

    Update fly.js

--- JS/fly.js
+++ JS/fly.js
@@ -1,5 +1,7 @@
 var body = $response.body;
 var obj = JSON.parse(body);
+delete obj.Variables.data.threaddetail.tagadv;
 delete obj.Variables.data.threaddetail.bottomadv;
 delete obj.Variables.data.threaddetail.middleadv;
+delete obj.Variables.data.threaddetail.pingyouadv;
 $done({body: JSON.stringify(obj)});
