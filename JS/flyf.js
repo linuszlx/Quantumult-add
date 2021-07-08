67957f82513bcaaed5799d552ba355b1d3118d9b
linuszlx <linuszhang@126.com>
2021-07-08 16:10:05 +0800

    Update flyf.js

--- JS/flyf.js
+++ JS/flyf.js
@@ -1,5 +1,6 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 delete obj.Variables.data.data;
+delete obj.Variables.data.home_popup_all;
 obj.Variables.msg = "暂无广告";
 $done({body: JSON.stringify(obj)});
