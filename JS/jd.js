5957236e2823cc0c383414b76f7b81a2b91e75e3
linuszlx <linuszhang@126.com>
2021-07-18 22:13:52 +0800

    Update jd.js

--- JS/jd.js
+++ JS/jd.js
@@ -1,5 +1,8 @@
 var body = $response.body;
 var obj = JSON.parse(body);
-obj.images = [];
+obj.images = [
+
+  ];
+obj.countdown = 100;
 obj.showTimesDaily  = 0;
 $done({body: JSON.stringify(obj)});
